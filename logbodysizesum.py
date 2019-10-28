ced51303985af92f9ba08bf35315c9f635611b6b
zabess <zabess@ucdavis.edu>
2019-10-28 12:17:38 -0700

    Calculates the sum of log body sizes and tallies diadromy across species

--- logbodysizesum.py
+++ logbodysizesum.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
-#This script reads in the Bloom_etal_2018_Reduced_Dataset displays the Taxa Name,
-#the diadromous status of the particular taxa, and it calculates the sum
+#This script reads in the Bloom_etal_2018_Reduced_Dataset, displays the Taxa Name,
+#the diadromous status of the particular taxa, and calculates the sum
 #of the log body sizes.
 InFile = open("Bloom_etal_2018_Reduced_Dataset.csv", 'r')
 #Read in the dataset.
@@ -12,7 +12,7 @@ nondiadromoustally = []
 #Initialize empty list.
 for Line in InFile:
 #Use a loop to strip the end-of-line break, splits the lines in the csv by commas,
-#print the taxa and its diadromous status and calculates the sum of all of the log body masses.
+#prints the taxa and its diadromous status and calculates the sum of all of the log body masses.
 	if LineNumber > 1:
 		Line = Line.strip('\n')
 		ElementList = Line.split(',')
@@ -24,10 +24,16 @@ for Line in InFile:
 			nondiadromoustally.append(1)
 	LineNumber = LineNumber + 1
 total = sum(tobesummed)
+#Sums the log body sizes
 print("Sum of all log body sizes: %s" %(total))
 #Prints the total log body size.
 totaldiadromous = sum(diadromoustally)
+#Sums the tally of 1's for diadromous species.
 print("Count of Diadromous species: %s" %(totaldiadromous))
+#Print the sum of diadromous species.
 totalnondiadromous = sum(nondiadromoustally)
+#Sums the nondiadromous tally.
 print("Count of Non-Diadromous species: %s" %(totalnondiadromous))
+#Prints the nondiadromous tally.
 InFile.close()
+#Closes the file.
